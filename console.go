762b8f311c48ab8d6a1be12bbdf8c788476399ed
Espen Zachrisen <Espen.Zachrisen@AlticeUSA.com>
2021-01-11 15:26:55 -0600

    Better console output

--- console.go
+++ console.go
@@ -2,6 +2,7 @@ package gcplog
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/sirupsen/logrus"
 )
@@ -11,7 +12,12 @@ type ConsoleFormatter struct {
 }
 
 func (f *ConsoleFormatter) Format(entry *logrus.Entry) ([]byte, error) {
+	var latency time.Duration
 
-	s := fmt.Sprintf("%-10s: %s\n", getGCPLevel(entry.Level), entry.Message)
+	if l, ok := entry.Data["latency"]; ok {
+		latency, _ = l.(time.Duration)
+	}
+
+	s := fmt.Sprintf("%-10s: %10s %s\n", getGCPLevel(entry.Level), latency, entry.Message)
 	return []byte(s), nil
 }
